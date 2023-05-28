8c526b948a599f2c03950a0dff3329eea667bc3f
EdrielSantos <https://github.com/EdrielSantos/ConstructionRiskAssessment.git>
2023-05-29 02:18:35 +0800

    CRUD done except for saving image and displaying to recyclerview

--- app/src/main/java/com/example/constructionriskassessment/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/constructionriskassessment/RecyclerViewAdapter.kt
@@ -3,23 +3,23 @@ package com.example.constructionriskassessment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.FrameLayout
-import android.widget.ImageView
+import android.widget.Button
 import android.widget.TextView
-import androidx.lifecycle.LiveData
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.example.constructionriskassessment.database.Report
 
 
-class RecyclerViewAdapter(): RecyclerView.Adapter<ViewHolder>() {
+
+class RecyclerViewAdapter(private val listener: ItemClickListener): RecyclerView.Adapter<ViewHolder>() {
     private val reportList = ArrayList<Report>()
+
 
     // This function instantiate the item_list layout and give ViewHolder the layout
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         val listItem = layoutInflater.inflate(R.layout.item_list, parent, false)
-        return ViewHolder(listItem)
+        return ViewHolder(listItem, listener)
     }
 
     // This function specifies how many rows of the data to be displayed
@@ -29,6 +29,9 @@ class RecyclerViewAdapter(): RecyclerView.Adapter<ViewHolder>() {
 
     // This function is the one that changes the values of a component in the ViewHolder(layout)
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.itemView.setOnClickListener {
+            listener.onItemClickListener(reportList[position])
+        }
         holder.bind(reportList[position])
     }
 
@@ -37,24 +40,32 @@ class RecyclerViewAdapter(): RecyclerView.Adapter<ViewHolder>() {
         reportList.addAll(reports)
     }
 
+    interface ItemClickListener{
+        fun onDeleteReportListener(report: Report)
+        fun onItemClickListener(report: Report)
+    }
+
+
 }
 
 // This class is the ViewHolder(layout) that will be displayed in the recyclerView
-class ViewHolder(private val view:View): RecyclerView.ViewHolder(view){
+class ViewHolder(private val view1:View, private val listener: RecyclerViewAdapter.ItemClickListener): RecyclerView.ViewHolder(view1){
 
-    fun bind(report: Report){
-        val myImageView = view.findViewById<ImageView>(R.id.list_img)
-        val myTextView = view.findViewById<TextView>(R.id.list_txt)
+    fun bind(report: Report) {
+        val myTextView = view1.findViewById<TextView>(R.id.list_txt)
+        val myHazardTv = view1.findViewById<TextView>(R.id.list_type)
+        val myDescriptionTv = view1.findViewById<TextView>(R.id.list_desc)
+        val mySevLvlTv = view1.findViewById<TextView>(R.id.list_sev)
+        val deleteButton = view1.findViewById<Button>(R.id.del_btn)
+
         myTextView.text = "Report ${report.id.toString()}"
-    }
+        myHazardTv.text = "${report.typeOfHazard.toString()}"
+        myDescriptionTv.text = "${report.description.toString()}"
+        mySevLvlTv.text = "${report.sev_level.toString()}"
 
-    private fun nav(){
-        view.findViewById<FrameLayout>(R.id.report_frame).setOnClickListener{
-            it.findNavController().navigate(R.id.action_reportListFragment_to_riskAssessmentFragment)
+        deleteButton.setOnClickListener {
+            listener.onDeleteReportListener(report)
         }
     }
-    // This is to initialize or run the function nav()
-    init {
-        nav()
-    }
+
 }
