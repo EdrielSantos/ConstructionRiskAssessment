4d9ae5326263a5ee270b6c7579596b08e8a3bbf8
ChristianRabadon <qcsrabadon@tip.edu.ph>
2023-05-29 21:44:39 +0800

    Image Upload Function Except saving in Database

--- app/src/main/java/com/example/constructionriskassessment/ReportFragment.kt
+++ app/src/main/java/com/example/constructionriskassessment/ReportFragment.kt
@@ -1,16 +1,15 @@
 
 package com.example.constructionriskassessment
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
 import android.os.Bundle;
 import android.view.LayoutInflater
 
 
 import android.view.View;
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.RadioGroup
-import android.widget.Toast
+import android.widget.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -24,47 +23,70 @@ class ReportFragment : Fragment() {
 	private lateinit var descriptionTxt: EditText
 	private lateinit var severityLvl: RadioGroup
 	private lateinit var viewModel: ReportViewModel
+	private lateinit var uploadBtn: Button
+	private lateinit var displayImage: ImageView
+	private lateinit var uploadTxt: TextView
+	private lateinit var imageUri: String
+	companion object {
+		val IMAGE_REQUEST_CODE = 100
+	}
+
 
 	override fun onCreateView(
 		inflater: LayoutInflater,
 		container: ViewGroup?,
 		savedInstanceState: Bundle?
 	): View? {
-	// Binding fragment_report layout
-	val binding = DataBindingUtil.inflate<FragmentReportBinding>(inflater,R.layout.fragment_report,container,false)
-	// Getting the instance of the database and getting the data access object (DA0) of that database
-	val dao = ReportDatabase.getInstance(requireActivity().application).reportDao()
-	// Calling ReportViewModelFactory that takes a constructor parameter DAO
-	val factory = ReportViewModelFactory(dao)
-	// ViewModel instance
-	viewModel = ViewModelProvider(this,factory).get(ReportViewModel::class.java)
+		// Binding fragment_report layout
+		val binding = DataBindingUtil.inflate<FragmentReportBinding>(inflater,R.layout.fragment_report,container,false)
+		// Getting the instance of the database and getting the data access object (DA0) of that database
+		val dao = ReportDatabase.getInstance(requireActivity().application).reportDao()
+		// Calling ReportViewModelFactory that takes a constructor parameter DAO
+		val factory = ReportViewModelFactory(dao)
+		// ViewModel instance
+		viewModel = ViewModelProvider(this,factory).get(ReportViewModel::class.java)
 
-	// Assigning the layout components to a global variable
-	submitBtn = binding.submitBtn
-	hazardTxt = binding.hazardEdTxt
-	descriptionTxt = binding.descriptionEdTxt
-	severityLvl = binding.radioBtn
+		// Assigning the layout components to a global variable
+		submitBtn = binding.submitBtn
+		hazardTxt = binding.hazardEdTxt
+		descriptionTxt = binding.descriptionEdTxt
+		severityLvl = binding.radioBtn
+		uploadBtn = binding.uploadBtn
+		displayImage = binding.displayImg
+		uploadTxt = binding.uploadTxt
 
 
-	submitBtn.setOnClickListener {
-		if(hazardTxt.text.isEmpty() || descriptionTxt.text.isEmpty())
-		{
-			Toast.makeText(context, "Please fill in the blanks", Toast.LENGTH_SHORT).show()
+		submitBtn.setOnClickListener {
+			if(hazardTxt.text.isEmpty() || descriptionTxt.text.isEmpty())
+			{
+				Toast.makeText(context, "Please fill in the blanks", Toast.LENGTH_SHORT).show()
+			}
+			else{
+				saveReportData()
+				clearData()
+			}
 		}
-		else{
-			saveReportData()
-			clearData()
+		uploadBtn.setOnClickListener {
+			uploadImage()
+			uploadBtn.alpha = 0f
+			uploadBtn.isEnabled = true
+			uploadTxt.visibility = View.INVISIBLE
 		}
-	}
+		displayImage.setOnClickListener {
+			uploadImage()
+		}
 
-	// Returning the layout
-	return binding.root
+
+		// Returning the layout
+		return binding.root
 	}
 
 	// For saving the data on the database
 	private fun saveReportData(){
 		val hazard = hazardTxt.text.toString()
 		val description = descriptionTxt.text.toString()
+		val image = imageUri.toString()
+
 		val severityLevel = when(severityLvl.checkedRadioButtonId){
 			R.id.low_btn -> "Low"
 			R.id.moderate_btn -> "Moderate"
@@ -81,8 +103,26 @@ class ReportFragment : Fragment() {
 		hazardTxt.setText("")
 		descriptionTxt.setText("")
 	}
+	private fun uploadImage() {
+		val intent = Intent(Intent.ACTION_PICK)
+		intent.type = "image/*"
+		startActivityForResult(intent, IMAGE_REQUEST_CODE)
+	}
+
+	override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+		super.onActivityResult(requestCode, resultCode, data)
+		if (requestCode == IMAGE_REQUEST_CODE && resultCode == RESULT_OK) {
+			val selectedImageUri = data?.data
+			if (selectedImageUri != null) {
+				displayImage.visibility = View.VISIBLE
+				displayImage.setImageURI(selectedImageUri)
+				displayImage.layoutParams.width = displayImage.width
+				displayImage.layoutParams.height = displayImage.height
+				imageUri = selectedImageUri.toString()
+			}
+		}
+	}
 
 
 }
-	
-	
+
