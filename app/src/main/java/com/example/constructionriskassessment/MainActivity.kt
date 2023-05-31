e3e40645dd5f0e793de287486b77f058afabfdb8
EdrielSantos <https://github.com/EdrielSantos/ConstructionRiskAssessment.git>
2023-05-31 17:04:29 +0800

    Final project

--- app/src/main/java/com/example/constructionriskassessment/MainActivity.kt
+++ app/src/main/java/com/example/constructionriskassessment/MainActivity.kt
@@ -3,14 +3,30 @@ package com.example.constructionriskassessment
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.constructionriskassessment.database.ReportDatabase
 import com.example.constructionriskassessment.databinding.ActivityMainBinding
 import com.example.constructionriskassessment.databinding.FragmentLoginBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this,R.layout.activity_main)
+
+        drawerLayout = binding.drawerLayout
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        NavigationUI.setupWithNavController(binding.navView, navController)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)          // This is for arrow back button
+        return NavigationUI.navigateUp(navController,drawerLayout)
     }
 }
